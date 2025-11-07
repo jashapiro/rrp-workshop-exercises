b0b8d9ec7cd60a5f06cba60c44506cb6ee4a9fd0
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2025-11-07 10:04:56 -0500

    Add script to download SRP255885 R1/R2 fastq files

---
+++ scripts/download-fastq.sh
@@ -0,0 +1,52 @@
+#!/bin/bash 
+
+# Set error handling for the script
+set -euo pipefail
+
+# Set the working directory to THIS FILE's directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# "Constant" variables
+study_id="SRP255885"
+fastq_url="ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889"
+
+# Define files and directories
+fastq_r1="SRR11518889_1.fastq.gz"
+fastq_dir="../data/raw/fastq/$study_id"
+
+# Create fastq directory
+mkdir -p $fastq_dir
+
+#### Obtain and "process" the R1 fastq file
+
+echo "Obtaining ${fastq_r1}" # this is going to print
+
+# download the file
+# the -O flag keeps the original internet file name
+curl -O $fastq_url/$fastq_r1
+
+# Explore the file: How many lines?
+echo "The number of lines in $fastq_r1 is...drumroll........"
+gunzip -c $fastq_r1 | wc -l
+
+# Move the fastq file to its home
+mv $fastq_r1 $fastq_dir
+
+
+##### Obtain R2
+fastq_r2="SRR11518889_2.fastq.gz"
+
+echo "Obtaining ${fastq_r2}" # this is going to print
+
+# download the file
+# the -O flag keeps the original internet file name
+curl -O $fastq_url/$fastq_r2
+
+# Explore the file: How many lines?
+echo "The number of lines in $fastq_r2 is...drumroll........"
+gunzip -c $fastq_r2 | wc -l
+
+# Move the fastq file to its home
+mv $fastq_r2 $fastq_dir
+
+
